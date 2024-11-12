bf1117a7bbdc607616c2e857be5aeb3c3c5e1416
michelia <michelia@seal.io>
2024-11-12 15:15:19 +0800

    add faster-whisper benchmark

--- scripts/my_benchmark_faster_whisper.py
+++ scripts/my_benchmark_faster_whisper.py
@@ -1,48 +1,35 @@
 import argparse
 import platform
-import subprocess
 from whisper.normalizers import EnglishTextNormalizer
 from whisper.normalizers import BasicTextNormalizer
 import time
 import os
+from faster_whisper.transcribe import BatchedInferencePipeline, WhisperModel
 from tqdm import tqdm
 import pandas as pd
 from pydub import AudioSegment
 import jiwer
 
-
 # run
-# conda create -n whisper-benchmark-whispercpp python=3.11
-# conda activate whisper-benchmark-whispercpp
+# conda create -n whisper-benchmark-faster-whisper python=3.11
+# conda activate whisper-benchmark-faster-whisper
 # cd scripts
-# pip install -r ../benchmark_requirements_whisper_cpp.txt --ignore-installed
+# conda install -c conda-forge pynini=2.1.5
+# pip install -r ../benchmark_faster_whipser_requirements_mac.txt --ignore-installed
 # mac:
-#   python my_benchmark_whisper_cpp.py --repo_path ../data --end_line 1 --device mps --binary_path /Users/michelia/Documents/project4ai/whisper.cpp/main --model_path /Users/michelia/Documents/project4ai/whisper.cpp/models/ggml-large-v2.bin
-# nvidia
-#   需要另外build whisper.cpp
-#   cd whisper.cpp
-#   sh ./models/download-ggml-model.sh large-v2
-#   make clean
-#   GGML_CUDA=1 make -j
-#   ./main -m ./models/ggml-large-v2.bin -f samples/jfk.wav -otxt jfk.txt
-#   python my_benchmark_whisper_cpp.py --repo_path ../data --end_line 1 --device cuda --binary_path /home/michelia/code/whisper.cpp/main --model_path /home/michelia/code/whisper.cpp/models/ggml-large-v2.bin
-# npu
-#   需要另外build whisper.cpp
-#   mkdir build
-#   cd build
-#   cmake .. -D GGML_CANN=on
-#   make -j
-#   ./main -m ./models/ggml-large-v2.bin -f samples/jfk.wav -otxt jfk.txt
-#   python my_benchmark_whisper_cpp.py --repo_path ../data --end_line 1 --device cuda --binary_path /home/michelia/code/whisper.cpp/main --model_path /home/michelia/code/whisper.cpp/models/ggml-large-v2.bin
-# amd
-#   看不出到底支不支持 https://github.com/ggerganov/whisper.cpp/pull/862 https://github.com/ggerganov/whisper.cpp/pull/2102 https://github.com/ggerganov/whisper.cpp/issues/2055
+#   python my_benchmark_faster_whisper.py --repo_path ../data --end_line 1 --device cpu --compute_type float32
+# nvidia:
+#   install cudadnn https://developer.nvidia.com/cudnn-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=deb_network
+#   pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/cu124
+#   python my_benchmark_faster_whisper.py --repo_path ../data --end_line 1 --device cuda --compute_type float32
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--binary_path', default="", type=str)
-    parser.add_argument('--model_path', default="", type=str)
     parser.add_argument('--repo_path', default="", type=str)
     parser.add_argument('--end_line', default=1, type=int)
+    parser.add_argument('--batch_size', default=16, type=int)
+    parser.add_argument('--compute_type', default="float16", type=str)
     parser.add_argument('--device', default="cuda", type=str)
     parser.add_argument('--threads', default=8, type=int)
 
@@ -50,14 +37,18 @@ def parse_arguments():
     return args
 
 
-def run(repo_path, end_line=1, device="cuda", threads=8, binary_path="", model_path="", lang="en"):
+def run(repo_path, end_line=1, compute_type="float16", batch_size=16, threads=8, device="cuda", lang="en"):
     system = platform.system()
 
-    results_dir = f"{repo_path}/results/WhisperCPP-bs-{system}-{device}-threads{threads}"
+    results_dir = f"{repo_path}/results/Faster-Whisper-bs_{batch_size}-{system}-{device}-{compute_type}-threads{threads}"
     os.makedirs(results_dir, exist_ok=True)
 
+    model = WhisperModel(
+        "large-v2", device, cpu_threads=threads, compute_type=compute_type)
+    batched_model = BatchedInferencePipeline(model=model)
+
     data = pd.read_csv(
-        f'{repo_path}/kincaid46_subset/kincaid46-wav.txt', sep=" ")
+        f'{repo_path}/kincaid46_subset/kincaid46.txt', sep=" ")
     files = [f"{repo_path}/{fn}" for fn in data['audio_path'].tolist()]
     text_files = [f"{repo_path}/{fn}" for fn in data['audio_text'].tolist()]
 
@@ -74,9 +65,16 @@ def run(repo_path, end_line=1, device="cuda", threads=8, binary_path="", model_p
     pred_text_list = []
     pred_begin_time = time.time()
     for fn in tqdm(files[:end_line], desc="audio trascribing"):
-        output = run_command(binary_path, model_path, fn, device, threads)
+        segments, info = batched_model.transcribe(
+            fn,
+            language=lang,
+            beam_size=1,
+            without_timestamps=True,
+            batch_size=batch_size)
 
-        normalize_result = normalizer(output.strip())
+        texts = [segment.text for segment in segments]
+        normalize_result = normalizer(" ".join([_.strip()
+                                                for _ in texts]))
 
         audio_segment = AudioSegment.from_file(fn)
         duration_sec = len(audio_segment) / 1000.0
@@ -134,11 +132,6 @@ def run(repo_path, end_line=1, device="cuda", threads=8, binary_path="", model_p
     infer_time = pd.DataFrame(infer_time[1:], columns=infer_time[0])
     infer_time.to_csv(f"{results_dir}/infer_time.tsv", sep="\t", index=False)
 
-# ref: https://colab.research.google.com/drive/1B8BtVepMyvlFuQQyv87AWKn_UNkav6xu?usp=sharing#scrollTo=PcTuPy-fvxJp
-# ref: https://github.com/g8a9/multilingual-asr-gender-gap/blob/800a1824adf66c95cbd367795976c6926dc419fa/src/2_sample_and_compute_metrics_binary.py#L28
-# https://whisperapi.com/word-error-rate-wer
-# https://www.educative.io/answers/how-to-compute-word-error-rate-wer-with-openai-whisper
-
 
 def calculate_metrics(references, transcriptions):
     return {
@@ -147,49 +140,14 @@ def calculate_metrics(references, transcriptions):
     }
 
 
-def run_command(binary_path: str, model_path: str, audio_file_path: str, device: str, threads: int) -> str:
-    command = [
-        binary_path,
-        "-m",
-        model_path,
-        "-f",
-        audio_file_path,
-        "-debug",
-        "--threads",
-        str(threads),
-        "--no-timestamps",
-    ]
-
-    if device == "cpu":
-        command.append("-ng")
-
-    try:
-        result = subprocess.run(
-            command, capture_output=True, text=True, check=True, encoding="utf-8"
-        )
-
-        if result.returncode != 0:
-            raise Exception(f"Unexpected return code: {result.returncode}")
-
-        output = result.stdout
-        if output == "" or output is None:
-            raise Exception(
-                f"Output is empty, return code: {result.returncode}")
-
-        return output
-    except Exception as e:
-        raise Exception(
-            f"Failed to execute {command}: {e},"
-            f" stdout: {result.stdout}, stderr: {result.stderr}"
-        )
-
-
 if __name__ == '__main__':
     args = parse_arguments()
-    run(args.repo_path,
-        binary_path=args.binary_path,
-        model_path=args.model_path,
+    run(
+        args.repo_path,
+        batch_size=args.batch_size,
         end_line=args.end_line,
+        compute_type=args.compute_type,
         device=args.device,
         threads=args.threads,
-        lang="en")   # for mac use fp32
+        lang="en")
+    # for mac set device to cpu
